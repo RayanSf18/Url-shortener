0d1477017c773e017eb981121afa58f3cd7a05e2
Rayan Silva <rayansf18@gmail.com>
2025-01-13 16:54:31 -0300

    Refactor App.java: improve code readability and organization

--- redirect-url-shortener/src/main/java/com/rayan/urlshortener/App.java
+++ redirect-url-shortener/src/main/java/com/rayan/urlshortener/App.java
@@ -12,54 +12,69 @@ import java.util.Map;
 
 public class App implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
-    private final S3Client s3Client = S3Client.builder().build();
 
+    private static final String BUCKET_NAME = "ubbzh-url-shortener-storage-lambda";
+    private static final String ERROR_FETCHING_OBJECT = "Error fetching object from S3: ";
+    private static final String ERROR_DESERIALIZING_OBJECT = "Error deserializing object data from S3: ";
+    private static final int STATUS_CODE_GONE = 410;
+    private static final int STATUS_CODE_FOUND = 302;
+
+    private final S3Client s3Client = S3Client.builder().build();
     private final ObjectMapper objectMapper = new ObjectMapper();
+
 
     @Override
     public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
 
+        String shortUrlCode = extractShortUrlCode(request);
+        InputStream s3ObjectStream = fetchS3Object(shortUrlCode);
+        UrlData urlData = deserializeUrlData(s3ObjectStream);
+        return createResponse(urlData);
+    }
+
+
+    private String extractShortUrlCode(Map<String, Object> request) {
         String pathParameters = (String) request.get("rawPath");
         String shortUrlCode = pathParameters.replace("/", "");
-
         if (shortUrlCode == null || shortUrlCode.isEmpty()) {
             throw new IllegalArgumentException("Invalid request: 'shortUrlCode' is required.");
         }
+        return shortUrlCode;
+    }
 
+
+    private InputStream fetchS3Object(String shortUrlCode) {
         GetObjectRequest obj = GetObjectRequest.builder()
-                .bucket("ubbzh-url-shortener-storage-lambda")
+                .bucket(BUCKET_NAME)
                 .key(shortUrlCode + ".json")
                 .build();
-
-        InputStream s3ObjectStream;
-
         try {
-            s3ObjectStream = s3Client.getObject(obj);
-        } catch (Exception exception){
-            throw new RuntimeException("Error fetching object from S3: " + exception.getMessage(), exception);
+            return s3Client.getObject(obj);
+        } catch (Exception exception) {
+            throw new RuntimeException(ERROR_FETCHING_OBJECT + exception.getMessage(), exception);
         }
+    }
 
-        UrlData urlData;
 
+    private UrlData deserializeUrlData(InputStream s3ObjectStream) {
         try {
-            urlData = objectMapper.readValue(s3ObjectStream, UrlData.class);
-        } catch (Exception exception){
-            throw new RuntimeException("Error deserializing object data from S3: " + exception.getMessage(), exception);
+            return objectMapper.readValue(s3ObjectStream, UrlData.class);
+        } catch (Exception exception) {
+            throw new RuntimeException(ERROR_DESERIALIZING_OBJECT + exception.getMessage(), exception);
         }
+    }
 
+
+    private Map<String, Object> createResponse(UrlData urlData) {
         long currentTimeInSeconds = System.currentTimeMillis() / 1000;
-
         Map<String, Object> response = new HashMap<>();
-
         if (urlData.getExpirationTime() < currentTimeInSeconds) {
-            response.put("statusCode", 410);
+            response.put("statusCode", STATUS_CODE_GONE);
             response.put("body", "This URL has expired.");
-            return response;
+        } else {
+            response.put("statusCode", STATUS_CODE_FOUND);
+            response.put("headers", Map.of("Location", urlData.getOriginalUrl()));
         }
-
-        response.put("statusCode", 302);
-        response.put("headers", Map.of("Location", urlData.getOriginalUrl()));
-
         return response;
     }
 }
