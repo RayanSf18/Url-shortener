c90885d90ced1ab00497c07f386bc04fe2c0a385
Rayan Silva <rayansf18@gmail.com>
2025-01-13 15:03:50 -0300

    feat: checks URL expiration and performs redirection

--- redirect-url-shortener/src/main/java/com/rayan/urlshortener/App.java
+++ redirect-url-shortener/src/main/java/com/rayan/urlshortener/App.java
@@ -7,16 +7,17 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
-public class App implements RequestHandler<Map<String, Object>, Map<String, String>> {
+public class App implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
     private final S3Client s3Client = S3Client.builder().build();
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public Map<String, String> handleRequest(Map<String, Object> request, Context context) {
+    public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
 
         String pathParameters = (String) request.get("rawPath");
         String shortUrlCode = pathParameters.replace("/", "");
@@ -46,7 +47,19 @@ public class App implements RequestHandler<Map<String, Object>, Map<String, Stri
             throw new RuntimeException("Error deserializing object data from S3: " + exception.getMessage(), exception);
         }
 
+        long currentTimeInSeconds = System.currentTimeMillis() / 1000;
 
-        return null;
+        Map<String, Object> response = new HashMap<>();
+
+        if (urlData.getExpirationTime() < currentTimeInSeconds) {
+            response.put("statusCode", 410);
+            response.put("body", "This URL has expired.");
+            return response;
+        }
+
+        response.put("statusCode", 302);
+        response.put("headers", Map.of("Location", urlData.getOriginalUrl()));
+
+        return response;
     }
 }
