7f81af6e6a75c7ab0b53421a40979caf0614e19b
Rayan Silva <rayansf18@gmail.com>
2025-01-12 23:52:23 -0300

    object deserialization

--- create-url-lambda/src/main/java/com/rayan/urlshortener/App.java
+++ create-url-lambda/src/main/java/com/rayan/urlshortener/App.java
@@ -2,14 +2,23 @@ package com.rayan.urlshortener;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Map;
 
 public class App implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public Map<String, String> handleRequest(Map<String, Object> request, Context context) {
         String body = (String) request.get("body");
+        Map<String, String> bodyData;
+        try {
+            bodyData = objectMapper.readValue(body, Map.class);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
+        }
         return null;
     }
 }
