ecdf971705d36c2f1678e32bec29ce138612cec9
Rayan Silva <rayansf18@gmail.com>
2025-01-12 23:59:39 -0300

    Extracting essential data from the lambda body

--- create-url-lambda/src/main/java/com/rayan/urlshortener/App.java
+++ create-url-lambda/src/main/java/com/rayan/urlshortener/App.java
@@ -13,12 +13,20 @@ public class App implements RequestHandler<Map<String, Object>, Map<String, Stri
     @Override
     public Map<String, String> handleRequest(Map<String, Object> request, Context context) {
         String body = (String) request.get("body");
+
         Map<String, String> bodyData;
+
         try {
             bodyData = objectMapper.readValue(body, Map.class);
         } catch (Exception exception) {
             throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
         }
+
+        String originalUrl = bodyData.get("url");
+        String expirationTime = bodyData.get("expirationTime");
+
+
+
         return null;
     }
 }
