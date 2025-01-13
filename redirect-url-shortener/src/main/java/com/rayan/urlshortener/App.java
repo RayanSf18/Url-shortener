d5ee7ed0dccbb78f5998648d0d641b1aa33f5512
Rayan Silva <rayansf18@gmail.com>
2025-01-13 12:19:37 -0300

    Getting the uuid of the path and implementing validation.

--- redirect-url-shortener/src/main/java/com/rayan/urlshortener/App.java
+++ redirect-url-shortener/src/main/java/com/rayan/urlshortener/App.java
@@ -8,7 +8,14 @@ import java.util.Map;
 public class App implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
     @Override
-    public Map<String, String> handleRequest(Map<String, Object> stringObjectMap, Context context) {
+    public Map<String, String> handleRequest(Map<String, Object> request, Context context) {
+
+        String pathParameters = (String) request.get("rawPath");
+        String shortUrlCode = pathParameters.replace("/", "");
+
+        if (shortUrlCode == null || shortUrlCode.isEmpty()) {
+            throw new IllegalArgumentException("Invalid request: 'shortUrlCode' is required.");
+        }
 
         return null;
     }
