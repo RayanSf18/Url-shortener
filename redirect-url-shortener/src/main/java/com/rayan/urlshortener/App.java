675b0c256cf0176a565669b625bd67b1ee2a54be
Rayan Silva <rayansf18@gmail.com>
2025-01-13 14:33:35 -0300

    getting an object from S3 bucket with error handling

--- redirect-url-shortener/src/main/java/com/rayan/urlshortener/App.java
+++ redirect-url-shortener/src/main/java/com/rayan/urlshortener/App.java
@@ -2,10 +2,15 @@ package com.rayan.urlshortener;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
+import java.io.InputStream;
 import java.util.Map;
 
 public class App implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    private final S3Client s3Client = S3Client.builder().build();
 
     @Override
     public Map<String, String> handleRequest(Map<String, Object> request, Context context) {
@@ -17,7 +22,21 @@ public class App implements RequestHandler<Map<String, Object>, Map<String, Stri
             throw new IllegalArgumentException("Invalid request: 'shortUrlCode' is required.");
         }
 
-        
+        GetObjectRequest obj = GetObjectRequest.builder()
+                .bucket("ubbzh-url-shortener-storage-lambda")
+                .key(shortUrlCode + ".json")
+                .build();
+
+        InputStream s3ObjectStream;
+
+        try {
+            s3ObjectStream = s3Client.getObject(obj);
+        } catch (Exception exception){
+            throw new RuntimeException("Error fetching object from S3: " + exception.getMessage(), exception);
+        }
+
+
+
 
         return null;
     }
